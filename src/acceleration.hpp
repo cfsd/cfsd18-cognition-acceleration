07b79275ff06cab96ed0b72245983752541047c3
linarn <linus.arno@gmail.com>
2018-07-16 18:17:22 +0200

    Clean up, Add velocity function and start stop logic + bugfix

--- src/acceleration.hpp
+++ src/acceleration.hpp
@@ -44,48 +44,23 @@ class Acceleration {
   Eigen::MatrixXf orderCones(Eigen::MatrixXf localPath);
   void Cartesian2Spherical(float, float, float, opendlv::logic::sensation::Point &);
   std::tuple<float, float> driverModelSteering(Eigen::MatrixXf);
-  float driverModelVelocity(Eigen::MatrixXf, float, float, float, float, float, float, float, bool, bool);
+  float driverModelVelocity(float, float);
 
   /* commandlineArguments */
   cluon::OD4Session &m_od4;
   int m_senderStamp{221};
-  // path
-  double m_receiveTimeLimit{0.01};
-  float m_distanceBetweenPoints{0.5f};
-  bool m_traceBack{false};
   // steering
   bool m_moveOrigin{true};
-  bool m_curveFitPath{true};
-  float m_previewTime{0.3f};
-  float m_minPrevDist{1.0f};
   float m_steerRate{50.0f};
-  float m_previewTimeSlam{0.3f};
-  float m_minPrevDistSlam{1.0f};
-  float m_steerRateSlam{50.0f};
-  //sharp
-  bool m_sharp{false};
-  int m_nSharp{10};
-  float m_K1{0.17f};
-  float m_Ky{0.5f};
-  float m_C{m_K1};
-  float m_c{1.0f};
   // velocity control
-  float m_diffToBrakeVel{0.5f};
-  float m_critDiff{0.1f};
-  float m_critDiff2{0.1f};
-  float m_accFreq{10};
-  float m_axSpeedProfile{-1.0f};
-  bool m_useAyReading{true};
+  bool m_useAyReading{false};
   float m_velocityLimit{5.0f};
   float m_mu{0.9f};
   float m_axLimitPositive{5.0f};
   float m_axLimitNegative{-5.0f};
   float m_headingErrorDependency{0.7f};
-  float m_curveDetectionAngle{1.0f};
-  int m_curveDetectionPoints{20};
   //....controller
   float m_aimVel{5.0f};
-  float m_keepConstVel{-1.0f};
   float m_aKp{0.1f};
   float m_aKd{0.0f};
   float m_aKi{0.0f};
@@ -95,12 +70,6 @@ class Acceleration {
   float m_sKp{0.1f};
   float m_sKd{0.0f};
   float m_sKi{0.0f};
-  // curvature estimation
-  bool m_polyFit{false};
-  int m_step{5};
-  int m_polyDeg{3};
-  int m_pointsPerSegment{15};
-  bool m_segmentizePolyfit{false};
   // vehicle specific
   float m_wheelAngleLimit{20.0f};
   float m_wheelBase{1.53f};
@@ -112,19 +81,18 @@ class Acceleration {
   std::mutex m_groundSpeedMutex;
   float m_lateralAcceleration;
   std::mutex m_lateralAccelerationMutex;
-  std::chrono::time_point<std::chrono::system_clock> m_tick;
-  std::chrono::time_point<std::chrono::system_clock> m_tock;
   std::chrono::time_point<std::chrono::system_clock> m_tickDt;
   std::chrono::time_point<std::chrono::system_clock> m_tockDt;
   std::chrono::time_point<std::chrono::system_clock> m_steerTickDt;
   std::chrono::time_point<std::chrono::system_clock> m_steerTockDt;
-  bool m_newClock;
-  bool m_specCase;
+  float m_ei;
+  float m_ePrev;
+  bool m_changeState;
   float m_prevHeadingRequest;
-  bool m_slamActivated;
-  bool m_paramsUpdated;
+  bool m_accelerate;
+  bool m_start;
+  bool m_STOP;
   std::mutex m_sendMutex;
-  const double RAD2DEG = 57.295779513082325; // 1.0 / DEG2RAD
 };
 
 #endif
